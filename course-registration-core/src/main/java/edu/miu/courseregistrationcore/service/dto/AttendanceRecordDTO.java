322a7bdad3ba79bf8061f5efa8a5e46dfa811805
Yalemsew Mekuriaw <yalemone@gmail.com>
2024-03-07 16:13:41 -0800

    clarification in attendance dto

--- course-registration-core/src/main/java/edu/miu/courseregistrationcore/service/dto/AttendanceRecordDTO.java
+++ course-registration-core/src/main/java/edu/miu/courseregistrationcore/service/dto/AttendanceRecordDTO.java
@@ -6,8 +6,10 @@ import java.time.LocalDateTime;
 
 @Data
 public class AttendanceRecordDTO {
+    // private long id;
     private LocalDateTime scanDateTime;
     private String locationName;
+    //locationType: class room, lab, library, etc.
     private String locationType;
     private String studentName;
 }
