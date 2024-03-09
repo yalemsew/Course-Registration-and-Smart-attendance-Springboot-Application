82c3773813ebaa433d0aba4701639b8cc9dce6a4
Yalemsew Mekuriaw <yalemone@gmail.com>
2024-03-09 00:07:22 -0800

    openApiConConfig file

---
+++ course-registration-core/src/main/java/edu/miu/courseregistrationcore/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package edu.miu.courseregistrationcore.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Course Registration Core REST API")
+                        .version("1.0")
+                        .description("Course Registration Core API Documentation"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
