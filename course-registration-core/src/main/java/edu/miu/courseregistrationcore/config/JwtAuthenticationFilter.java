1269c6e33d2803283d86beaba286eb989e261444
Yalemsew Mekuriaw <yalemone@gmail.com>
2024-03-26 22:46:25 -0700

    refactor security class

--- course-registration-core/src/main/java/edu/miu/courseregistrationcore/config/JwtAuthenticationFilter.java
+++ course-registration-core/src/main/java/edu/miu/courseregistrationcore/config/JwtAuthenticationFilter.java
@@ -23,9 +23,11 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver handlerExceptionResolver;
 
+
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
+    // Constructor
     public JwtAuthenticationFilter(
             JwtService jwtService,
             UserDetailsService userDetailsService,
